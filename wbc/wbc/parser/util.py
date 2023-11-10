6c007fe7a39d0c730376eb4c7ad3938dc0afaba9
Matt Jadud <matthew.jadud@gsa.gov>
2023-11-10 07:07:19 -0500

    Renaming.

---
+++ wbc/wbc/parser/util.py
@@ -0,0 +1,40 @@
+from .exceptions import ParseException
+
+def check_type(o, type):
+    if "type" not in o:
+        raise ParseException(f"Object does not contain key `type`")
+    if o['type'] != type:
+        raise ParseException(f"Object is not of type `{type}`")
+    return True
+    
+def requires_keys(o, keys):
+    for r in keys:
+        if r not in o:
+            raise ParseException(f"missing key {r} in sheet")
+    return True
+
+def allowed_keys(o, keys):
+    for k in o.keys():
+        if k not in keys:
+            raise ParseException(f"key {k} not allowed in object {o}")
+        
+
+def excel_from_number(column: int) -> str:
+    col_string = ""
+    col_num = column
+    while col_num > 0:
+        current_letter_number = (col_num - 1) % 26
+        current_letter = chr(current_letter_number + 65)
+        col_string = current_letter + col_string
+        col_num = (col_num - (current_letter_number + 1)) // 26
+    return col_string
+
+
+def number_from_excel(column: str) -> int:
+    total = 0
+    # Fancy notation to reverse a string is str[::-1]
+    for ndx, letter in enumerate(column[::-1]):
+        letter_val = (ord(letter) - 65) + 1
+        val = letter_val * (26**ndx)
+        total += val
+    return total
